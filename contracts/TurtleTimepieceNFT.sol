f6a7c7cffb4257e1dcab657ee022ba4f81392351
ZAC <zachery.johnson.dev@gmail.com>
2025-06-18 00:34:19 +0900

    Update the SC source

--- contracts/TurtleTimepieceNFT.sol
+++ contracts/TurtleTimepieceNFT.sol
@@ -1,105 +1,88 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "hardhat/console.sol";
 
 /**
  * @title TurtleTimepieceNFT
  * @dev ERC721 contract for the Turtle Timepiece NFT collection
  */
-contract TurtleTimepieceNFT is ERC721, Ownable, ReentrancyGuard, Pausable {
+contract TurtleTimepieceNFT is ERC721URIStorage, Ownable, ReentrancyGuard, Pausable {
     using Counters for Counters.Counter;
     using Strings for uint256;
     Counters.Counter private _tokenIds;
     
     // Maximum supply of NFTs
-    uint256 public constant MAX_SUPPLY = 20;
-    
-    // Base URI for metadata
-    string private _baseTokenURI;
+    uint256 public constant MAX_SUPPLY = 1;
     
     // Price per NFT in ETH
-    uint256 public mintPrice = 0.114 ether;
+    uint256 public mintPrice = 0.001 ether;
+    
+    // IPFS folder CID
+    string private constant IPFS_FOLDER = "QmUxwjKEFoWAmeCQfRBxhVsDar9CMpqBtCEHgpgW3nEE8M";
     
     // Mapping to track which tokens are available for sale
     mapping(uint256 => bool) public isTokenForSale;
-
-    // Mapping to track pre-purchased tokens (via Stripe)
-    mapping(uint256 => address) public stripePurchases;
     
     // Events
-    event NFTMinted(address owner, uint256 tokenId);
+    event NFTMinted(address owner, uint256 tokenId, string tokenURI);
     event NFTSold(address from, address to, uint256 tokenId, uint256 price);
     event TokenPutForSale(uint256 tokenId);
     event TokenRemovedFromSale(uint256 tokenId);
-    event TokenPrePurchased(uint256 tokenId, address purchaser);
     event TokenClaimed(uint256 tokenId, address claimer);
     event ContractPaused(address indexed by);
     event ContractUnpaused(address indexed by);
     
-    constructor() ERC721("Timeless Experience", "Turtle") {
-        _baseTokenURI = "";
-        // Pre-mint all NFTs to the contract owner
+    constructor() ERC721("Aquaduct", "Timeless") {
+        // Pre-mint NFTs to the contract owner
         address owner = _msgSender();
-        for (uint256 i = 1; i <= MAX_SUPPLY; i++) {
+        for(uint256 i = 1; i <= MAX_SUPPLY; i++) {
             _tokenIds.increment();
             _mint(owner, i);
-            isTokenForSale[i] = true; // Mark all tokens as available for sale
-            emit NFTMinted(owner, i);
+            
+            // Construct the full URI with .json extension
+            string memory uri = string(
+                abi.encodePacked(
+                    "ipfs://",
+                    IPFS_FOLDER,
+                    "/",
+                    Strings.toString(i),
+                    ".json"
+                )
+            );
+            
+            console.log("Setting URI for token %s: %s", Strings.toString(i), uri);
+            _setTokenURI(i, uri);
+            isTokenForSale[i] = true; // Mark token as available for sale
+            emit NFTMinted(owner, i, uri);
         }
     }
     
     /**
-     * @dev Returns the base URI for token metadata
+     * @dev Returns the URI for a given token ID
      */
-    function _baseURI() internal view override returns (string memory) {
-        return _baseTokenURI;
-    }
-    
-    /**
-     * @dev Removes the "ipfs://" prefix from a string
-     */
-    function _removeIpfsPrefix(string memory str) internal pure returns (string memory) {
-        bytes memory strBytes = bytes(str);
-        require(strBytes.length >= 7 && 
-                strBytes[0] == "i" && 
-                strBytes[1] == "p" && 
-                strBytes[2] == "f" && 
-                strBytes[3] == "s" && 
-                strBytes[4] == ":" && 
-                strBytes[5] == "/" && 
-                strBytes[6] == "/", 
-                "Invalid IPFS URI");
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721URIStorage) returns (string memory) {
+        require(_exists(tokenId), "Token does not exist");
+        require(tokenId <= MAX_SUPPLY, "Token ID exceeds maximum supply");
         
-        bytes memory result = new bytes(strBytes.length - 7);
-        for(uint i = 7; i < strBytes.length; i++) {
-            result[i-7] = strBytes[i];
-        }
-        return string(result);
-    }
-    
-    /**
-     * @dev Returns the URI for a given token ID with HTTP gateway URL
-     */
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        _requireMinted(tokenId);
-
-        string memory baseURI = _baseURI();
-        require(bytes(baseURI).length > 0, "Base URI not set");
-        return string.concat(baseURI, tokenId.toString(), ".json");
-    }
-    
-    /**
-     * @dev Updates the base URI for token metadata
-     * @param baseURI New base URI
-     */
-    function setBaseURI(string memory baseURI) external onlyOwner {
-        _baseTokenURI = baseURI;
+        string memory uri = string(
+            abi.encodePacked(
+                "ipfs://",
+                IPFS_FOLDER,
+                "/",
+                Strings.toString(tokenId),
+                ".json"
+            )
+        );
+        
+        console.log("Returning URI for token %s: %s", Strings.toString(tokenId), uri);
+        return uri;
     }
     
     /**
@@ -143,47 +126,30 @@ contract TurtleTimepieceNFT is ERC721, Ownable, ReentrancyGuard, Pausable {
     }
 
     /**
-     * @dev Mark a token as pre-purchased via Stripe
-     * @param tokenId The token that was purchased
-     * @param purchaser The address that will be able to claim the token
-     */
-    function markTokenAsPrePurchased(uint256 tokenId, address purchaser) external onlyOwner {
-        require(_exists(tokenId), "Token does not exist");
-        require(isTokenForSale[tokenId], "Token is not for sale");
-        require(purchaser != address(0), "Invalid purchaser address");
-        require(stripePurchases[tokenId] == address(0), "Token already pre-purchased");
-        
-        isTokenForSale[tokenId] = false;
-        stripePurchases[tokenId] = purchaser;
-        emit TokenPrePurchased(tokenId, purchaser);
-    }
-
-    /**
-     * @dev Claim a pre-purchased token
+     * @dev Claim a token (for Stripe purchases)
      * @param tokenId The token to claim
      */
     function claimToken(uint256 tokenId) external nonReentrant whenNotPaused {
         require(_exists(tokenId), "Token does not exist");
-        require(stripePurchases[tokenId] == msg.sender, "Not authorized to claim");
+        require(isTokenForSale[tokenId], "Token is not available for claiming");
         
         address tokenOwner = ownerOf(tokenId);
         require(tokenOwner == owner(), "Token not owned by contract owner");
         
         _transfer(tokenOwner, msg.sender, tokenId);
-        stripePurchases[tokenId] = address(0);
+        isTokenForSale[tokenId] = false;
         
         emit TokenClaimed(tokenId, msg.sender);
     }
 
     /**
-     * @dev Purchase an NFT that is for sale with ETH
+     * @dev Purchase an NFT with ETH
      * @param tokenId The token ID to purchase
      */
     function purchaseNFT(uint256 tokenId) external payable nonReentrant whenNotPaused {
         require(_exists(tokenId), "Token does not exist");
         require(isTokenForSale[tokenId], "Token is not for sale");
         require(msg.value >= mintPrice, "Insufficient ETH sent");
-        require(stripePurchases[tokenId] == address(0), "Token is pre-purchased");
         
         address seller = ownerOf(tokenId);
         require(seller != msg.sender, "Cannot buy your own token");
@@ -225,7 +191,7 @@ contract TurtleTimepieceNFT is ERC721, Ownable, ReentrancyGuard, Pausable {
         uint256 count = 0;
         
         for (uint256 i = 1; i <= _tokenIds.current(); i++) {
-            if (isTokenForSale[i] && stripePurchases[i] == address(0)) {
+            if (isTokenForSale[i]) {
                 tokensForSale[count] = i;
                 count++;
             }
@@ -239,13 +205,4 @@ contract TurtleTimepieceNFT is ERC721, Ownable, ReentrancyGuard, Pausable {
         
         return result;
     }
-
-    /**
-     * @dev Check if a token is pre-purchased and by whom
-     * @param tokenId The token ID to check
-     * @return The address that pre-purchased the token, or zero address if not pre-purchased
-     */
-    function getPrePurchaser(uint256 tokenId) external view returns (address) {
-        return stripePurchases[tokenId];
-    }
-}
+} 
